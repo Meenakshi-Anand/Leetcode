f62822f8351c0ed6df08d9de2d824e7a1433cf35
Meenakshi <abhi@codeanand.com>
2019-09-17 14:00:00 +0100

    Delete Node In A Linked List

---
+++ delete_node_in_a_linked_list.rb
@@ -0,0 +1,30 @@
+# QUESTION: 237
+# Write a function to delete a node (except the tail) in a singly linked list, given only access to that node.
+# Given linked list -- head = [4,5,1,9], which looks like following:
+#
+# Example 1:
+#
+# Input: head = [4,5,1,9], node = 5
+# Output: [4,1,9]
+# Explanation: You are given the second node with value 5, the linked list should become 4 -> 1 -> 9 after calling your function.
+# Example 2:
+#
+# Input: head = [4,5,1,9], node = 1
+# Output: [4,5,9]
+# Explanation: You are given the third node with value 1, the linked list should become 4 -> 5 -> 9 after calling your function.
+
+# Definition for singly-linked list.
+# class ListNode
+#     attr_accessor :val, :next
+#     def initialize(val)
+#         @val = val
+#         @next = nil
+#     end
+# end
+
+# @param {ListNode} node
+# @return {Void} Do not return anything, modify node in-place instead.
+def delete_node(node)
+    node.val = node.next.val
+    node.next = node.next.next
+end
