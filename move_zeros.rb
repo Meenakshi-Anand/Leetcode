bd7518df07012f2433d23dd042a2f35a3b75a44c
Meenakshi <abhi@codeanand.com>
2019-11-09 18:54:41 -0800

    move zeros

---
+++ move_zeros.rb
@@ -0,0 +1,29 @@
+# question no : 283
+
+def move_zeroes(nums)
+    i = 0
+    loop_count = 0
+    while (i < nums.length)
+        if(nums[i] == 0)
+
+            if(i==0)
+
+            nums.shift;
+            nums.push(0);
+
+            else
+
+            nums.slice!(i)
+            nums.push(0);
+
+            end
+
+            i=i-1;
+            return nums if loop_count > nums.length
+
+        end
+        loop_count = loop_count+1;
+        i=i+1;
+    end
+    nums
+end
